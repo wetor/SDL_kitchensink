f7874efaba2aa7b34ecca00e07a07384e87328a7
wetor <2929339419@qq.com>
2019-08-14 23:52:36 +0800

    upload

---
+++ build/CMakeFiles/SDL_kitchensink.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/audio/kitaudio.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/audio/kitaudio.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/kitdecoder.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/kitdecoder.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/kitlibstate.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/kitlibstate.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/libass.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/libass.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/kitatlas.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/kitatlas.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/kitsubtitle.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/kitsubtitle.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/kitsubtitlepacket.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/kitsubtitlepacket.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/renderers/kitsubass.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/renderers/kitsubass.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/renderers/kitsubimage.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/renderers/kitsubimage.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/subtitle/renderers/kitsubrenderer.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/subtitle/renderers/kitsubrenderer.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/utils/kitbuffer.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/utils/kitbuffer.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/utils/kithelpers.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/utils/kithelpers.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/utils/kitringbuffer.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/utils/kitringbuffer.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/internal/video/kitvideo.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/internal/video/kitvideo.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/kiterror.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/kiterror.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/kitlib.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/kitlib.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/kitplayer.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/kitplayer.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/kitsource.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/kitsource.c.obj"
+  "/home/li/ONScripter-Switch/SDL_kitchensink/src/kitutils.c" "/home/li/ONScripter-Switch/SDL_kitchensink/build/CMakeFiles/SDL_kitchensink.dir/src/kitutils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "KIT_VERSION_MAJOR=1"
+  "KIT_VERSION_MINOR=0"
+  "KIT_VERSION_PATCH=7"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/internal"
+  "../src/internal/audio"
+  "../src/internal/subtitle"
+  "../src/internal/subtitle/renderers"
+  "../src/internal/utils"
+  "../src/internal/video"
+  "../include"
+  "/opt/devkitpro/portlibs/switch/include/SDL2"
+  "/opt/devkitpro/portlibs/switch/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
